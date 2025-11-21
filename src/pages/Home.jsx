8cf857829c2cc06d9397f005212e282df2181f0d
Razik <razik3407@gmail.com>
2025-11-21 22:29:32 +0530

    updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
-import HeroSection from '../sections/heroSection/HeroSection'
+
 import AboutSection from '../sections/AboutSection/AboutSection'
 import FeaturedCategories from '../sections/FeaturedCategories/FeaturedCategories'
 import WhyChooseUsSection from '../sections/WhyChooseUs/WhyChooseUsSection'
 import CtaSection from '../sections/CTA/CtaSection'
 import Footer from '../sections/FooterSection/Footer'
+import HeroSection from '../sections/heroSection/HeroSection'
 
 const Home = () => {
   return (
